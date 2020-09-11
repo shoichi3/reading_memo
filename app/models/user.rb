3d344e5f173b00a533f21ce736907c310ec70c3a
shoichi3 <dece12shou@gmail.com>
2020-09-11 14:11:30 +0900

    モデルの設定

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :user_books
+  has_many :books, through: :user_books
+  has_many :memos
+
+  validates :name, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  with_options presence: true, format: { with: PASSWORD_REGEX, message: 'Include both letters and numbers' } do
+    validates :password
+  end
+end
