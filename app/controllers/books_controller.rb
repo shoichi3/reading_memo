502cbd359911b7039cf42a79edb32b1c2c95c711
shoichi3 <dece12shou@gmail.com>
2020-09-14 17:44:55 +0900

    コントローラーの設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
 
+  before_action :get_book, only: [:show]
+
   def new
     @book = Book.new
   end
@@ -13,9 +15,17 @@ class BooksController < ApplicationController
     end
   end
 
+  def show
+  end
+
   private
 
   def book_params
     params.require(:book).permit(:title).merge(user_id: current_user.id)
   end
+
+  def get_book
+    @book = Book.find(params[:id])
+  end
+
 end
