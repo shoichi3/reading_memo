30ae65d59d6b8d8631b43514c8f07dd8f6b2176a
shoichi3 <dece12shou@gmail.com>
2020-09-14 14:20:16 +0900

    bookモデルの設定

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,8 @@
 class Book < ApplicationRecord
 
-  has_many :user_books
-  has_many :users, through: :user_books
+  # has_many :user_books
+  # has_many :users, through: :user_books
+  belongs_to :user
   has_many :memos
 
   validates :title, presence: true
