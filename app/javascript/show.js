86fa2ff1b33d3991bd30e4f4b8fb0df280de2ebb
shoichi3 <dece12shou@gmail.com>
2020-09-15 09:09:30 +0900

    プルダウンメニューの作成

--- app/javascript/show.js
+++ app/javascript/show.js
@@ -1,14 +1,32 @@
-function pullDown() {
-  const pullDownButton = document.getElementById('lists');
-  const pullDownParent = document.getElementById('pull-down');
+// function pullDown() {
+//   const pullDownButton = document.getElementById('lists');
+// console.log(pullDownButton);
+//   const pullDownParent = document.getElementById('pull-down');
   
-  pullDownButton.addEventListener('click', function() {
-    if (pullDownParent.getAttribute("style") == "display:block;") {
-      pullDownParent.removeAttribute("style", "display:block;");
-    } else {
-      pullDownParent.setAttribute("style", "display:block;");
+//   pullDownButton.addEventListener('click', function() {
+//     if (pullDownParent.getAttribute("style") == "display:block;") {
+//       pullDownParent.removeAttribute("style", "display:block;");
+//     } else {
+//       pullDownParent.setAttribute("style", "display:block;");
+//     }
+//   });
+// }
+
+// window.addEventListener('load', pullDown);
+
+(function () {
+  document.addEventListener('DOMContentLoaded', function() { // HTML解析が終わったら
+    const btn = document.getElementById('lists');
+    const pullDownParent = document.getElementById('pull-down');
+    // ボタンをidで取得
+    if(btn) { // ボタンが存在しないときにエラーになるのを回避
+      btn.addEventListener('click', function(){ //ボタンがクリックされたら
+        if (pullDownParent.getAttribute("style") == "display:block;") {
+          pullDownParent.removeAttribute("style", "display:block;");
+        } else {
+          pullDownParent.setAttribute("style", "display:block;");
+        }
+      });
     }
   });
-}
-
-window.addEventListener('load', pullDown);
+}());
