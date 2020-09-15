24765527d4d2bef632f73ae9af8f7bb58fc6e0a0
shoichi3 <dece12shou@gmail.com>
2020-09-15 12:15:44 +0900

    コントローラーの設定

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,2 +1,28 @@
 class MemosController < ApplicationController
+
+  before_action :get_book, only: [:new]
+
+  def new
+    @book = Book.find(params[:book_id])
+    @memo = Memo.new
+  end
+
+  def create
+    @memo = Memo.new(memo_params)
+    if @memo.save
+      redirect_to book_path(@memo.book)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def memo_params
+    params.require(:memo).permit(:image, :memo).merge(user_id: current_user.id, book_id: params[:book_id])
+  end
+
+  def get_book
+    @book = Book.find(params[:book_id])
+  end
 end
