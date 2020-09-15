03b96a4a171132c171b34870a7e3283d316c0489
shoichi3 <dece12shou@gmail.com>
2020-09-15 12:15:32 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root "users#index"
   resources :users, only: [:show]
-  resources :books, only: [:new, :create, :show, :edit, :update, :destroy] do
+  resources :books, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :books do
     resources :memos, only: [:new, :create]
   end
 end
