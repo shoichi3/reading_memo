dcf47c2265a177c408446e0f6c35e0006e33e383
shoichi3 <dece12shou@gmail.com>
2020-09-15 18:52:56 +0900

    dependent: :destoryの追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,7 @@ class Book < ApplicationRecord
   # has_many :user_books
   # has_many :users, through: :user_books
   belongs_to :user
-  has_many :memos
+  has_many :memos, dependent: :destroy
 
   validates :title, presence: true
 end
