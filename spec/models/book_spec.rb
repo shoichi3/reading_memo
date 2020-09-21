f19409c87a3b4ae28062a598e428d2d3a936f2e0
shoichi3 <dece12shou@gmail.com>
2020-09-22 08:25:37 +0900

    単体テストコードの記述

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @book = FactoryBot.build(:book)
+  end
+  describe '本追加機能' do
+    it '本のタイトルが入力されている場合、本を追加できる' do
+      expect(@book).to be_valid
+    end
+
+    it '本のタイトルが空白の場合、本を追加することはできない' do
+      @book.title = nil
+      @book.valid?
+      expect(@book.errors.full_messages).to include("Title can't be blank")
+    end
+  end
 end
--- spec/models/memo_spec.rb
+++ spec/models/memo_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Memo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @memo = FactoryBot.build(:memo)
+    @memo.image = fixture_file_upload('public/images/1491.jpg')
+  end
+
+  describe 'メモ追加機能' do
+    it '画像とメモが入力されている場合、メモを追加することができる' do
+      expect(@memo).to be_valid
+    end
+
+    it '画像が空の場合、メモを追加することはできない' do
+      @memo.image = nil
+      @memo.valid?
+      expect(@memo.errors.full_messages).to include("Image can't be blank")
+    end
+    
+    it 'メモが空の場合、メモを追加することはできない' do
+      @memo.memo = nil
+      @memo.valid?
+      expect(@memo.errors.full_messages).to include("Memo can't be blank")
+    end
+  end
 end
