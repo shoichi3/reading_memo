9b45023458a03eb4ced30d909e17a566b9c8f1c1
shoichi3 <dece12shou@gmail.com>
2020-10-09 15:22:09 +0900

    FactoryBotの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     name                    { '山田太郎' }
     email                   { Faker::Internet.free_email }
-    password                { Faker::Internet.password(min_length: 6) }
+    password                { 'tnamry3ye' }
     password_confirmation   { password }
   end
 end
