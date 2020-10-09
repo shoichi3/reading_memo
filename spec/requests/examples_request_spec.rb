974d425fbc344c43676fba4cdeb9fcdec9b9292e
shoichi3 <dece12shou@gmail.com>
2020-10-09 15:10:41 +0900

    結合テストコードの修正

--- spec/requests/examples_request_spec.rb
+++ spec/requests/examples_request_spec.rb
@@ -1,10 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe 'Examples', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/examples/index'
-      expect(response).to have_http_status(:success)
-    end
-  end
+
 end
--- spec/requests/illustrations_request_spec.rb
+++ spec/requests/illustrations_request_spec.rb
@@ -1,10 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe 'Illustrations', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/illustrations/index'
-      expect(response).to have_http_status(:success)
-    end
-  end
+
 end
